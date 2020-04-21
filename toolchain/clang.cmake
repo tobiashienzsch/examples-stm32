2874bf54e3c21cb7e38564813c522ebb6025b630
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-21 05:18:34 +0200

    Minor fixes

--- toolchain/clang.cmake
+++ toolchain/clang.cmake
@@ -11,9 +11,9 @@ set(TOOLCHAIN_PREFIX arm-none-eabi-)
 set(TOOLCHAIN_TRIPLE arm-none-eabi)
 
 execute_process(
-        COMMAND ${UTIL_SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
-        OUTPUT_VARIABLE BINUTILS_PATH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND ${UTIL_SEARCH_CMD} ${TOOLCHAIN_PREFIX}gcc
+    OUTPUT_VARIABLE BINUTILS_PATH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 get_filename_component(ARM_TOOLCHAIN_DIR ${BINUTILS_PATH} DIRECTORY)
@@ -38,14 +38,14 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # Optimizations
-set(CMAKE_C_FLAGS_DEBUG             "-Og -ggdb3 -DDEBUG" CACHE INTERNAL "c compiler flags debug")
-set(CMAKE_CXX_FLAGS_DEBUG           "-Og -ggdb3 -DDEBUG" CACHE INTERNAL "cxx compiler flags debug")
-set(CMAKE_ASM_FLAGS_DEBUG           "-ggdb3" CACHE INTERNAL "asm compiler flags debug")
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG    "" CACHE INTERNAL "linker flags debug")
+set(CMAKE_C_FLAGS_DEBUG             "-flto -Og -ggdb3 -DDEBUG" CACHE INTERNAL "c compiler flags debug")
+set(CMAKE_CXX_FLAGS_DEBUG           "-flto -Og -ggdb3 -DDEBUG" CACHE INTERNAL "cxx compiler flags debug")
+set(CMAKE_ASM_FLAGS_DEBUG           "-flto -ggdb3" CACHE INTERNAL "asm compiler flags debug")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG    "-fuse-ld=lld -flto" CACHE INTERNAL "linker flags debug")
 
 set(CMAKE_C_FLAGS_RELEASE           "-flto -Oz -DNDEBUG " CACHE INTERNAL "c compiler flags release")
 set(CMAKE_CXX_FLAGS_RELEASE         "-flto -Oz -DNDEBUG " CACHE INTERNAL "cxx compiler flags release")
-set(CMAKE_ASM_FLAGS_RELEASE         "" CACHE INTERNAL "asm compiler flags release")
+set(CMAKE_ASM_FLAGS_RELEASE         "-flto" CACHE INTERNAL "asm compiler flags release")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE  "-fuse-ld=lld -flto" CACHE INTERNAL "linker flags release")
 
 string(APPEND CMAKE_EXE_LINKER_FLAGS " -nostdlib")
