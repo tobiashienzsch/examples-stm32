265bd2ad61a34625d504f4db9b070d39f1c1cf43
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-21 05:41:31 +0200

    Remove unused comments

--- examples/gpio/Inc/stm32f1xx_hal_conf.h
+++ examples/gpio/Inc/stm32f1xx_hal_conf.h
@@ -1,23 +1,3 @@
-/**
- ******************************************************************************
- * @file    stm32f1xx_hal_conf.h
- * @author  MCD Application Team
- * @brief   HAL configuration file.
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F1xx_HAL_CONF_H
 #define __STM32F1xx_HAL_CONF_H
 
@@ -26,10 +6,6 @@ extern "C"
 {
 #endif
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-
-/* ########################## Module Selection ############################## */
 /**
  * @brief This is the list of modules to be used in the HAL driver
  */
